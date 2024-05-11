5316d2f8359fce01a9a5c8af6a1ab20b0ba46d3f
neeraj0304 <neerajsahu6644@gmail.com>
2024-05-11 17:06:57 +0530

    commit

--- javascript.js
+++ javascript.js
@@ -1,15 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleCheckbox = document.getElementById("checkbox");
+    const navbarLinks = document.querySelectorAll(".navbar a");
 
-  Shery.makeMagnet(".magnet" , {duration: 1,});
-
-  Shery.textAnimate(".text-target" /* Element to target.*/, {
-    //Parameters are optional.
-    style: 1,
-    y: 10,
-    delay: 0.1,
-    duration: 2,
-    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-    multiplier: 0.1,
-    
+    navbarLinks.forEach((link) => {
+      link.addEventListener("click", () => {
+        toggleCheckbox.checked = false;
+      });
+    });
   });
+
+  
 
   
